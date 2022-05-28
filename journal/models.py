b38962bc0a60a1edca4ae78203ed062e7cd1f772
Josh Moody <joshmoody24@gmail.com>
2022-05-28 06:37:27 +0000

    added basic tag and entry functionality

--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self):
+        return self.name
+
+class Entry(models.Model):
+    date = models.DateField()
+    tags = models.ManyToManyField(Tag)
+    content = models.TextField()
+
+    def __str__(self):
+        return str(self.date)
--- journal/views.py
+++ journal/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def indexPageView(request):
-    return HttpResponse("Hello Universe!")
+    entries = Entry.objects.all()
+    context = {"journal_entries": entries}
+    return render(request, "index.html", context)
